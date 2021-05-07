0dc52da56587956b6bbc08d5b2d731a9860ead22
Erick Yondon <eryondon@microsoft.com>
2021-05-07 11:19:01 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dockerIssue10840
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
